13d93e99b0b9fa35e1ffc1dda236c2c284258a5a
guicamher <guillecampher15@gmail.com>
2026-02-05 20:52:49 +0100

    a

--- src/main/kotlin/com/example/cochesMySQL/repository/DepreciacionRepository.kt
+++ src/main/kotlin/com/example/cochesMySQL/repository/DepreciacionRepository.kt
@@ -1,8 +1,11 @@
 package com.example.cochesMySQL.repository
 
 import com.example.cochesMySQL.model.Depreciacion
+import com.example.cochesMySQL.model.Nota
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface DepreciacionRepository : JpaRepository<Depreciacion, Int>
+interface DepreciacionRepository : JpaRepository<Depreciacion, Int>{
+    fun findByCocheFk(cocheFk: Int): Depreciacion
+}
--- src/main/kotlin/com/example/cochesMySQL/service/DepreciacionService.kt
+++ src/main/kotlin/com/example/cochesMySQL/service/DepreciacionService.kt
@@ -1,6 +1,7 @@
 package com.example.cochesMySQL.service
 
 import com.example.cochesMySQL.model.Depreciacion
+import com.example.cochesMySQL.model.Nota
 import com.example.cochesMySQL.repository.DepreciacionRepository
 import org.springframework.stereotype.Service
 
@@ -12,6 +13,8 @@ class DepreciacionService(
     fun listarTodas(): List<Depreciacion> = repository.findAll()
 
     fun obtenerPorId(id: Int): Depreciacion? = repository.findById(id).orElse(null)
+
+    fun obtenerPorFk(cocheFk: Int): Depreciacion = repository.findByCocheFk(cocheFk)
 
     fun guardar(depreciacion: Depreciacion): Depreciacion = repository.save(depreciacion)
 
